82504724743880d3fa22296263cd6353d5d74fa1
njkim0529 <njkim0529@naver.com>
2015-06-16 15:09:11 +0900

    minor bug fixed

--- KnowledgeBase/src/elements/LogfileManagement.java
+++ KnowledgeBase/src/elements/LogfileManagement.java
@@ -19,7 +19,7 @@ public class LogfileManagement {
 	// Set Logfile Name with current Date and Time.txt
 	// For Mac OS X, set path like "/Users/.../logfile/
 	setLogfileName("/Users/nj/testlog/" + time.getCurrentTime() + ".txt");
-	// setLogfileName("C:\\logfile\\"+ getCurrentTime() + ".txt");
+	// setLogfileName("C:\\logfile\\"+ time.getCurrentTime() + ".txt");
 	logFile = new File(getLogfileName());
 	try {
 	    out = new FileOutputStream(logFile);
